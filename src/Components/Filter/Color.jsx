5afd0366cc8005a0459cddd0221fe68e55036fd8
Emma24968 <emnabs12@gmail.com>
2025-12-22 17:25:33 +0100

    I added my color component

---
+++ src/Components/Filter/Color.jsx
@@ -0,0 +1,23 @@
+import "./Color.css";
+export const Color = () => {
+  return (
+    <div className="color-filter">
+      <div className="row">
+        {" "}
+        
+        <div className="color-circle clear">×</div>
+        <div className="color-circle yellow"></div>
+        <div className="color-circle red"></div>
+        <div className="color-circle blue"></div>
+        <div className="color-circle green"></div>
+      </div>
+      <div className="row">
+        {" "}
+        <div className="color-circle black"></div>
+        <div className="color-circle cyan"></div>
+        <div className="color-circle pink"></div>
+        <div className="color-circle orange"></div>
+      </div>
+    </div>
+  );
+};
