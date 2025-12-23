941276b9c33490d561c5c8f7c21da3a25882e50a
Emma24968 <emnabs12@gmail.com>
2025-12-23 05:30:41 +0100

    I added my rating section

--- src/Components/Filter/Color.jsx
+++ src/Components/Filter/Color.jsx
@@ -1,20 +1,33 @@
 export const Color = () => {
   return (
     <div className="color-filter">
-      <div className="flex">
+      <h1 className="font-semibold">Color</h1>
+      <div className="flex gap-3">
         {" "}
-        <div className=" p-1 m-1 rounded-full w-3 h-3 border-2 cursor-pointer">X</div>
+        <div
+          className="
+    m-2
+    w-4 h-4
+    rounded-full
+    flex items-center justify-center
+     text-[15px]
+    shadow-[0_0_2px_rgba(0,0,0,7.3)]
+    cursor-pointer
+  "
+        >
+          X
+        </div>
         <div className="bg-red-700 p-2 m-2 rounded-full w-3 h-3 cursor-pointer"></div>
         <div className="bg-yellow-700 p-2 m-2 rounded-full w-3 h-3 cursor-pointer"></div>
         <input
           type="radio"
           name="category"
           defaultChecked
-          className="accent-blue-500"
+          className="accent-blue-500 w-4"
         />
         <div className="bg-indigo-100 p-2 m-2 rounded-full w-3 h-3 cursor-pointer"></div>
       </div>
-      <div className="row">
+      <div className="flex gap-3">
         {" "}
         <div className="bg-green-700 p-2 m-2 rounded-full w-3 h-3 cursor-pointer"></div>
         <div className="bg-cyan-700 p-2 m-2 w-3 h-3 rounded-full cursor-pointer">
--- src/Components/Filter/Rating.jsx
+++ src/Components/Filter/Rating.jsx
@@ -1,9 +1,46 @@
-export const Rating = ()=>{
-    return(
-        <div>
-            <div>
-                <input type="radio" name="" id="" /><label htmlFor="">5★</label>
-            </div>
+export const Rating = () => {
+  return (
+    <div className="grid ">
+      <div className="flex gap-4">
+        <div className="flex items-center gap-1">
+          <input type="radio" className="cursor-pointer" />
+          <label className="cursor-pointer flex items-center gap-1">
+            <span>5</span>
+            <span className="text-yellow-400 font-bold">★</span>
+          </label>
         </div>
-    )
-}
+        <div className="flex items-center gap-1">
+          <input type="radio" className="cursor-pointer" />
+          <label className="cursor-pointer flex items-center gap-1">
+            <span>4</span>
+            <span className="text-yellow-400 font-bold">★</span>
+          </label>
+        </div>
+        <div className="flex items-center gap-1">
+          <input type="radio" className="cursor-pointer" />
+          <label className="cursor-pointer flex items-center gap-1">
+            <span>3</span>
+            <span className="text-yellow-400 font-bold">★</span>
+          </label>
+        </div>
+      
+      </div>
+      <div className="flex gap-4">
+        <div className="flex items-center gap-1">
+          <input type="radio" className="cursor-pointer" />
+          <label className="cursor-pointer flex items-center gap-1">
+            <span>2</span>
+            <span className="text-yellow-400 font-bold">★</span>
+          </label>
+        </div>
+        <div className="flex items-center gap-1">
+          <input type="radio" className="cursor-pointer" />
+          <label className="cursor-pointer flex items-center gap-1">
+            <span>1</span>
+            <span className="text-yellow-400 font-bold">★</span>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
