2c54a4b19ed383af403a34206566d99918e12396
Emma24968 <emnabs12@gmail.com>
2025-12-23 04:29:57 +0100

    I updated my color section and added the colors

--- src/Components/Filter/Color.jsx
+++ src/Components/Filter/Color.jsx
@@ -1,21 +1,25 @@
 export const Color = () => {
   return (
     <div className="color-filter">
-      <div className="row">
+      <div className="flex">
         {" "}
-        
-        <div className="color-circle clear">×</div>
-        <div className="color-circle yellow"></div>
-        <div className="color-circle red"></div>
-        <div className="color-circle blue"></div>
-        <div className="color-circle green"></div>
+        <div className=" p-1 m-1 rounded-full w-3 h-3 cursor-pointer">X</div>
+        <div className="bg-red-700 p-2 m-2 rounded-full w-3 h-3 cursor-pointer"></div>
+        <div className="bg-yellow-700 p-2 m-2 rounded-full w-3 h-3 cursor-pointer"></div>
+        <div className="bg-indigo-100 p-2 m-2 rounded-full w-3 h-3 cursor-pointer"></div>
       </div>
       <div className="row">
         {" "}
-        <div className="color-circle black"></div>
-        <div className="color-circle cyan"></div>
-        <div className="color-circle pink"></div>
-        <div className="color-circle orange"></div>
+        <div className="bg-green-700 p-2 m-2 rounded-full w-3 h-3 cursor-pointer"></div>
+        <div className="bg-cyan-700 p-2 m-2 w-3 h-3 rounded-full cursor-pointer">
+          {" "}
+        </div>
+        <div className="bg-black p-2 m-2 w-3 h-3 rounded-full cursor-pointer">
+          {" "}
+        </div>
+        <div className="bg-pink-700 p-2 m-2 w-3 h-3 rounded-full cursor-pointer">
+          {" "}
+        </div>
       </div>
     </div>
   );
--- src/Components/Filter/Size.jsx
+++ src/Components/Filter/Size.jsx
@@ -1,18 +1,21 @@
-;
-
 export const Size = () => {
   return (
     <div>
       <h3 className="font-semibold">Size</h3>
       <div className="grid">
         <div className="flex gap-5">
-          <input type="radio" id="testing"/><label for="testing">XS</label>
-          <input type="radio" id="testing"/><label for="testing">X</label>
-          <input type="radio" id="testing"/><label for="testing">M</label>
+          <input type="radio" />
+          <label>XS</label>
+          <input type="radio" />
+          <label>X</label>
+          <input type="radio" />
+          <label>M</label>
         </div>
         <div className="flex gap-5">
-                    <input type="radio" id="testing"/><label for="testing">XL</label>
-                    <input type="radio" id="testing"/><label for="testing">L</label>
+          <input type="radio" />
+          <label>XL</label>
+          <input type="radio" />
+          <label>L</label>
         </div>
       </div>
     </div>
