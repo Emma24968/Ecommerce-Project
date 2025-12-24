07652acbf2bcc89754b110a358009d1d2d9c426a
Emma24968 <emnabs12@gmail.com>
2025-12-24 03:22:23 +0100

    I created a hero folder for my product

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -9,18 +9,18 @@ import { Size } from "./Size";
 
 export const Filter = () => {
   return (
-    <div className="w-[15%]">
+    <div className="w-[18%] bg-white rounded shadow p-4 sticky top-4 mt-4 mb-4">
       <Header />
       <Category />
-      <hr className='mt-3 mb-3' />
+      <hr className='mt-1.5 mb-1.5 border-gray-200' />
       <Size />
-      <hr className='mt-3 mb-3' />
+      <hr className='mt-1 mb-1  border-gray-200' />
       <Color />
 
-      <hr className='mt-3 mb-3' />
+      <hr className='mt-1 mb-1  border-gray-200' />
 
       <PriceRange />
-      <hr className='mt-3 mb-3' />
+      <hr className='mt-1 mb-1  border-gray-200' />
       <Rating />
     </div>
 
