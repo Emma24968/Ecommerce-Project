4ca90d6c8a1eb38a0e839ab27592c2d838776c54
Emma24968 <emnabs12@gmail.com>
2025-12-25 04:46:06 +0100

    I fixed the height and width bug on my card component

--- src/Components/Hero-Section/Card.jsx
+++ src/Components/Hero-Section/Card.jsx
@@ -1,31 +1,32 @@
 import cashmere from "../../assets/Media_Folder/cashmere.png";
+import techSleeve from "../../assets/Media_Folder/tech-short-sleeve.png"
 
 export const Card = ({title}) => {
   return (
-    <div className="Product-details w-[75%] bg-white m-10 p-4 rounded">
-      <div className="bg-gray-500 rounded">
-        {" "}
-        <img
-          src={cashmere}
-          alt="Cashmere"
-          className="w-[205px] h-[205px] mb-2 "
-        />
-      </div>
-      <div className="flex">
-        <div className="">
-          {" "}
-          <p  className="font-semibold">{title}</p>
-          <p>
-            Rating:<span className="text-yellow-400 font-bold">★★★★★</span>{" "}
-          </p>
-          <p>Available: 220</p>
-        </div>
-        <div className="">
-          <button className="w-10 mt-[40px] h-10 rounded bg-blue-800 text-[20px] text-white">
-            +
-          </button>
-        </div>
-      </div>
+   <div className="Product-details w-[75%] bg-white m-10 p-4 rounded">
+  <div className="bg-gray-500 rounded w-full h-[260px] flex items-center justify-center overflow-hidden">
+    <img
+      src={techSleeve}
+      alt="Cashmere"
+      className="w-full h-full object-contain"
+    />
+  </div>
+
+  <div className="flex justify-between mt-3">
+    <div>
+      <p className="font-semibold">{title}</p>
+      <p>
+        Rating:
+        <span className="text-yellow-400 font-bold">★★★★★</span>
+      </p>
+      <p>Available: 220</p>
     </div>
+
+    <button className="w-10 h-10 self-end rounded bg-blue-800 text-[20px] text-white">
+      +
+    </button>
+  </div>
+</div>
+
   );
 };
