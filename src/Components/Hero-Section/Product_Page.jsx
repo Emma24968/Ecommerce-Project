b374b5e0e8408bee7789d495d791399ff4447f5f
Emma24968 <emnabs12@gmail.com>
2025-12-25 05:30:23 +0100

    I added more product card

--- src/Components/Hero-Section/Product_Page.jsx
+++ src/Components/Hero-Section/Product_Page.jsx
@@ -1,16 +1,33 @@
-import { Card } from "./Card"
-import { Title } from "./Title"
+import { Card } from "./Card";
+import { Title } from "./Title";
 import cashmere from "../../assets/Media_Folder/cashmere.png";
-import techSleeve from "../../assets/Media_Folder/tech-short-sleeve.png"
-
-export const Product =()=>{
-    return(
-        <div className="flex">
-          <Title />  
-          <Card title={`Women's Tech Short Sleeve`} image={techSleeve}/>
-          <Card title={'Cashmere Wool Blend Coat'} image={cashmere}/>
-          <Card />
-
-        </div>
-    )
-}
+import techSleeve from "../../assets/Media_Folder/tech-short-sleeve.png";
+import fleece from "../../assets/Media_Folder/fleece-jogger.png";
+import waffle from "../../assets/Media_Folder/waffle.png";
+import short from "../../assets/Media_Folder/champions-short.png";
+import slimFit from "../../assets/Media_Folder/slimfit.png";
+import men_slimFit from "../../assets/Media_Folder/men-slim-fit.png";
+import men_champions from "../../assets/Media_Folder/men-champions.png";
+import men_tech from "../../assets/Media_Folder/men-tech.png";
+export const Product = () => {
+  return (
+    <div className="flex">
+      <Title />
+      <div>
+        <Card title={`Women's Tech Short Sleeve`} image={techSleeve} />
+        <Card title={"Cashmere Wool Blend Coat"} image={cashmere} />
+        <Card title={"French Terry Fleece Jogger"} image={fleece} />
+      </div>
+      <div>
+        <Card title={"Lightweight Waffle Full-Length"} image={waffle} />
+        <Card title={`Champion Women's Shorts`} image={short} />
+        <Card title={`Women's Slim-Fit`} image={slimFit} />
+      </div>
+      <div>
+        <Card title={`Men's Slim-Fit`} image={men_slimFit} />
+        <Card title={`Champion Men's Shorts`} image={men_champions} />
+        <Card title={`Men's Tech Short Sleeve`} image={men_tech} />
+      </div>
+    </div>
+  );
+};
