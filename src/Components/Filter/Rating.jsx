2c3365b92c7a675f6ab4d2c9474fb031abc6e493
Emma24968 <emnabs12@gmail.com>
2026-01-01 15:30:13 +0100

    I destructured my reting array

--- src/Components/Filter/Rating.jsx
+++ src/Components/Filter/Rating.jsx
@@ -1,46 +1,17 @@
+const ratings = [5, 4, 3, 2, 1];
+
 export const Rating = () => {
   return (
-    <div className="grid ">
-      <div className="flex gap-4">
-        <div className="flex items-center gap-1">
-          <input type="radio" className="cursor-pointer" />
-          <label className="cursor-pointer flex items-center gap-1">
-            <span>5</span>
+    <div className="grid grid-cols-3 ">
+      {ratings.map((rating) => (
+        <label key={rating}>
+          <input type="radio" value={rating} />
+          <span>
+            {rating}
             <span className="text-yellow-400 font-bold">★</span>
-          </label>
-        </div>
-        <div className="flex items-center gap-1">
-          <input type="radio" className="cursor-pointer" />
-          <label className="cursor-pointer flex items-center gap-1">
-            <span>4</span>
-            <span className="text-yellow-400 font-bold">★</span>
-          </label>
-        </div>
-        <div className="flex items-center gap-1">
-          <input type="radio" className="cursor-pointer" />
-          <label className="cursor-pointer flex items-center gap-1">
-            <span>3</span>
-            <span className="text-yellow-400 font-bold">★</span>
-          </label>
-        </div>
-      
-      </div>
-      <div className="flex gap-4">
-        <div className="flex items-center gap-1">
-          <input type="radio" className="cursor-pointer" />
-          <label className="cursor-pointer flex items-center gap-1">
-            <span>2</span>
-            <span className="text-yellow-400 font-bold">★</span>
-          </label>
-        </div>
-        <div className="flex items-center gap-1">
-          <input type="radio" className="cursor-pointer" />
-          <label className="cursor-pointer flex items-center gap-1">
-            <span>1</span>
-            <span className="text-yellow-400 font-bold">★</span>
-          </label>
-        </div>
-      </div>
+          </span>
+        </label>
+      ))}
     </div>
   );
 };
