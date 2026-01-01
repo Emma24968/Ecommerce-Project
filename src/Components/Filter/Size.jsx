23ab52b5d5306e7df97d9f4af5fab67b0e10c516
Emma24968 <emnabs12@gmail.com>
2026-01-01 15:15:37 +0100

    I destructured my size array

--- src/Components/Filter/Size.jsx
+++ src/Components/Filter/Size.jsx
@@ -1,22 +1,23 @@
+const sizes = ["XS", "S", "M", "L", "XL"];
+
 export const Size = () => {
   return (
     <div>
-      <h3 className="font-semibold">Size</h3>
-      <div className="grid">
-        <div className="flex gap-5">
-          <input type="radio" />
-          <label>XS</label>
-          <input type="radio" />
-          <label>X</label>
-          <input type="radio" />
-          <label>M</label>
-        </div>
-        <div className="flex gap-5">
-          <input type="radio" />
-          <label>XL</label>
-          <input type="radio" />
-          <label>L</label>
-        </div>
+      <h3 className="font-semibold mb-2">Size</h3>
+
+      <div className="grid grid-cols-3 gap-2">
+        {sizes.map((size) => (
+          <label
+            key={size}
+          >
+            <input
+              type="radio"
+              value={size}
+              className="accent-black"
+            />
+            <span>{size}</span>
+          </label>
+        ))}
       </div>
     </div>
   );
